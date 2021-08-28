c74bde70bbb46cf85fb334655323ef61a84f02bf
Joseph <josephsasson1@gmail.com>
2021-08-28 11:56:50 -0400

    beginning of project

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [user, setUser] = useState(true);
+  
+  if (!user) {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <div>
+              <Link to={"/"}>
+                SocialBookie
+              </Link>
+              <div>
+                <ul>
+                  <li>
+                    <Link to={"/login"}>
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={"/signup"}>
+                      Sign up
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          <div>
+            <div>
+              <Switch>
+                <Route
+                  path="/login"
+                  // component={() => <Login />}
+                />
+                <Route
+                  path="/signup"
+                  // component={() => <Signup />}
+                />
+                <Route 
+                  path="/"
+                  // component={() => <Login />}
+                />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <div>
+            <Link to={"/"}>
+              SocialBookie
+            </Link>
+            <div>
+              <ul>
+                <li>
+                  <Link to={"/home"}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/profile"}>
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/"}>
+                    Logout
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div>
+          <div>
+            <Switch>
+              <Route
+                path="/home"
+                // component={() => (<Home />)}
+              />
+              <Route
+                path="/profile"
+                // component={() => (<Profile />)}
+              />
+              <Route
+                path="/"
+                // component={() => (<Home />)}
+              />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
