56f40c863d91067b5ac1f55782ed3b53c3fc77d4
Joseph <josephsasson1@gmail.com>
2021-09-12 00:15:35 -0400

    update mybets

--- client/src/component/MyBets.js
+++ client/src/component/MyBets.js
@@ -12,28 +12,22 @@ function MyBets({user}){
     .then(setBetslips)
   },[])
 
-  const toggleActive = () => {
-    setActive(!active)
-
-  }
-
   return (
     <div>
       <br/>
         <div className = 'button'>
-          <button className = 'active-button' onClick = {toggleActive}>{active ? 'Active Bets' : 'Settled Bets'}</button>
+          <button className = 'active-button' onClick = {()=>setActive(!active)}>{active ? 'Active Bets' : 'Settled Bets'}</button>
         </div>
+        <div className="row">
           {active ?
-            <div className="row">
-              {betslips.filter(betslip=>betslip.user.id === user.id)
+              betslips.filter(betslip=>betslip.user.id === user.id)
               .filter(bet=>bet.winner === null)
-                .map(betslip=>{return <Betslip key={betslip.id} betslip={betslip}/>})}
-            </div> : 
-            <div className="row">
-              {betslips.filter(betslip=>betslip.user.id === user.id)
+                .map(betslip=>{return <Betslip key={betslip.id} betslip={betslip}/>})
+            : 
+              betslips.filter(betslip=>betslip.user.id === user.id)
               .filter(bet=>bet.winner === true || bet.winner === false)
                 .map(betslip=>{return <Betslip key={betslip.id} betslip={betslip}/>})}
-      </div>}
+        </div>
     </div>
   )
 }
